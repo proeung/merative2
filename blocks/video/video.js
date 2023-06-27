800b2a17db7dcc56ad9cffdea3c3047f71256acd
Putra Bonaccorsi <putra.roeung@gmail.com>
2023-06-27 16:00:26 -0400

    feat(iframe): fix lint

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -117,13 +117,13 @@ const loadYouTubePlayer = (element, videoId) => {
   // Create a new iframe element
   const iframe = document.createElement('iframe');
   iframe.src = `https://www.youtube.com/embed/${videoId}`;
-  
+
   // Add the data-hj-allow-iframe attribute
   iframe.setAttribute('data-hj-allow-iframe', '');
 
   // Append the iframe to the specified element
   element.appendChild(iframe);
-  
+
   // we have to create a new YT Player but then need to wait for its onReady event
   // before assigning it to the player map
   // eslint-disable-next-line no-new
