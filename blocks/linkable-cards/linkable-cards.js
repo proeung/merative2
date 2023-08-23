2f403fa741ddae483708b0a753e1646ad4a5db78
Putra Bonaccorsi <putra.roeung@gmail.com>
2023-08-23 17:02:25 -0400

    feat(product-event-template): add stats with illustration style and linkable block

--- blocks/linkable-cards/linkable-cards.js
+++ blocks/linkable-cards/linkable-cards.js
@@ -4,12 +4,19 @@ import { setRowDetails } from '../related-resources/related-resources.js';
 
 export default async function decorate(block) {
   let html = '';
+
+  const blockName = block.getAttribute('data-block-name');
+  if (!blockName) {
+    return;
+  }
+
   if ([...block.children].length > 0) {
     [...block.children].forEach((element) => {
       const content = element.children[0].innerHTML;
       const lastColumn = element.children[element.children.length - 1];
       const link = lastColumn.querySelectorAll('a')[0].getAttribute('href');
-      html += `<div><a href="${link}">${content}</a></div>`;
+
+      html += `<div class="${blockName}__item"><a href="${link}">${content}</a></div>`;
       block.innerHTML = html;
     });
   } else { // fetchign documents and populating top 3 cards.
