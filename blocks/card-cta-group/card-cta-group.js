da35ba7f27c78670cfa1d50581d7bb36f9a3187a
Putra Bonaccorsi <putra.roeung@gmail.com>
2023-09-15 15:39:08 -0400

    feat(card-cta): add markup and style changes

---
+++ blocks/card-cta-group/card-cta-group.js
@@ -0,0 +1,18 @@
+import { toClassName } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  const blockName = block.getAttribute('data-block-name');
+  if (!blockName) {
+    return;
+  }
+
+  [...block.children].forEach((element) => {
+    element.classList.add(`${blockName}__item`);
+
+    // Find last column
+    const lastColumn = element.querySelectorAll('div:last-child');
+    const themeClass = toClassName(lastColumn[0].innerText);
+
+    element.setAttribute('data-theme', themeClass);
+  });
+}
